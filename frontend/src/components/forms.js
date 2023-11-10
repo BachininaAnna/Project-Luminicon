c77aa05a89314915bb1ac821b675c2346ea406ca
BachininaAnna <125889059+BachininaAnna@users.noreply.github.com>
2023-11-11 00:05:57 +0300

    fixes-1

--- frontend/src/components/forms.js
+++ frontend/src/components/forms.js
@@ -1,58 +1,6 @@
-class Form {
-    constructor(page) {
-        this.btnEnter = null;
-        this.fields = [
-            {
-                name: 'fullName',
-                id: 'fullName',
-                element: null,
-                regex: /^[А-Я][а-я]+s*/,
-                valid: false,
-            },
-            {
-                name: 'email',
-                id: 'email',
-                element: null,
-                regex: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-                valid: false,
-            },
-            {
-                name: 'password',
-                id: 'password',
-                element: null,
-                regex: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/,
-                valid: false,
-            }
-        ];
+let forms = document.querySelectorAll('.needs-validation')
 
-        const that = this;
-        this.fields.forEach(item => {
-            item.element = document.getElementById(item.id);
-            item.element.onchange = function () {
-                that.validateField.call(that, item, this);
-            }
-        });
-
-        this.btnEnter = document.getElementById('btnEnter');
-        this.btnEnter.onclick = function () {
-
-        }
-    }
-
-    validateField(field, element) {
-        if (!element.value || !element.value.match(field.regex)) {
-            element.parentNode.style.borderColor = 'red';
-            field.valid = false;
-        } else {
-            element.parentNode.removeAttribute('style');
-            field.valid = true;
-        }
-    }
-}
-const inputs = [...document.getElementsByClassName('form-control')];
-//const inputs = [...document.querySelectorAll('.needs-validation')[0]];
-
-/*Array.prototype.slice.call(forms)
+Array.prototype.slice.call(forms)
     .forEach(function (form) {
         form.addEventListener('submit', function (event) {
             if (!form.checkValidity()) {
@@ -62,17 +10,4 @@ const inputs = [...document.getElementsByClassName('form-control')];
 
             form.classList.add('was-validated')
         }, false)
-    })*/
-
-inputs.forEach(input => {
-    input.oninput = function () {
-        if (input.value) {
-            input.classList.add('is-valid');
-            console.log('true');
-        } else {
-            input.classList.add('is-invalid');
-        }
-        console.log(input.value);
-    };
-})
-console.log(inputs);
+    })
