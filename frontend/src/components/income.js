3be7a693fb4a3cda926421b3c39c3a91f607e1a9
BachininaAnna <125889059+BachininaAnna@users.noreply.github.com>
2023-11-12 18:01:27 +0300

    fixes-2

--- frontend/src/components/income.js
+++ frontend/src/components/income.js
@@ -1,3 +1,4 @@
 import {Popup} from "../utilis/popup.js";
 
 Popup.show();
+
---
+++ frontend/src/utilis/popup.js
@@ -0,0 +1,25 @@
+export class Popup {
+    static show() {
+        const buttonsDelete = document.querySelectorAll('button.delete');
+        const modal = document.getElementById('modal');
+
+        const buttonConfirm = document.getElementById('modal-confirm');
+        const buttonCancel = document.getElementById('modal-cancel');
+
+        if (buttonsDelete && modal) {
+            buttonsDelete.forEach(item => {
+                item.on('click', function () {
+                    modal.classList.add('d-block');
+                })
+            })
+            buttonCancel.addEventListener('click', function () {
+                modal.classList.remove('d-block');
+            })
+
+            buttonConfirm.addEventListener('click', function () {
+                modal.classList.remove('d-block');
+                /*--*/
+            })
+        }
+    }
+}
