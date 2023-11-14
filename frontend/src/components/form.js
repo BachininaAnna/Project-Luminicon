258d23038001669ecd498a5b2416ee883e402c1e
BachininaAnna <125889059+BachininaAnna@users.noreply.github.com>
2023-11-14 12:43:12 +0300

    http-server

---
+++ frontend/src/components/form.js
@@ -0,0 +1,20 @@
+export class Form {
+    constructor() {
+        this.form = document.querySelectorAll('.needs-validation');
+    }
+
+
+    validateForm(){
+        Array.prototype.slice.call(this.form)
+            .forEach(function (field) {
+                field.addEventListener('submit', function (event) {
+                    if (!field.checkValidity()) {
+                        event.preventDefault()
+                        event.stopPropagation()
+                    }
+                    field.classList.add('was-validated')
+                }, false)
+            })
+    }
+}
+
--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,10 +1,50 @@
+import {Form} from "./components/form.js";
+
 export class Router {
     constructor() {
         this.routes = [
             {
                 route: '#/',
                 title: 'Главная',
+                template: 'templates/main.html',
+                styles: 'styles/index.css',
+                load: () => {
+
+                }
+            },
+            {
+                route: '#/form',
+                title: 'Регистрация',
+                template: 'templates/signup.html',
+                styles: 'styles/index.css',
+                load: () => {
+                    new Form();
+                }
+            },
+            {
+                route: '#/income',
+                title: 'Доходы',
+                template: 'templates/income.html',
+                styles: 'styles/index.css',
+                load: () => {
+
+                }
             }
         ]
+    }
+
+    async openRoute(){
+        const newRoute = this.routes.find(item => {
+            return item.route === window.location.hash;
+        })
+        if (!newRoute) {
+            window.location.href = '#/form';
+            return;
+        }
+
+        document.getElementById('content').innerHTML =
+            await fetch(newRoute.template).then(response => response.text());
+        document.getElementById('title').innerText = newRoute.title;
+        newRoute.load();
     }
 }
